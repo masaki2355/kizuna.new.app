a851e10ebc8de9daf6314f62002b8c228b7004fe
masaki2355 <takoyaki2355@icloud.com>
2022-03-10 22:02:10 +0900

    createアクション追加

--- src/app/controllers/tweets_controller.rb
+++ src/app/controllers/tweets_controller.rb
@@ -9,6 +9,16 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  def create
+    @tweet = Tweet.new(params.require(:tweet).permit(:schoolyear, :event).merge(user_id: current_user.id))
+
+    if @tweet.save
+      redirect_to tweets_path(@tweet)
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 end
